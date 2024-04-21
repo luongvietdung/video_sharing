feeacae5b02b95344c5ebc5b3fd5b74a08ba8b12
luongvietdung <dung.lv1992@gmail.com>
2024-04-21 11:12:49 +0700

    Init again

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Flowbite, ThemeModeScript } from "flowbite-react";
+import { Inter } from "next/font/google";
+import { type FC, type PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+import "./globals.css";
+import { flowbiteTheme } from "./theme";
+
+
+export const metadata: Metadata = {
+  title: "Youtube Sharing Video",
+  description: "Created by Dung LV",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+const RootLayout: FC<PropsWithChildren> = function ({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <ThemeModeScript />
+      </head>
+      <body className={twMerge("bg-gray-50 dark:bg-gray-900", inter.className)}>
+        <Flowbite theme={{ theme: flowbiteTheme }}>{children}</Flowbite>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
